e4d7223e4f1f336952c822f8937e5714f2acea44
AkshatJain <jainakshat_02@protonmail.com>
2025-07-04 02:43:56 +0530

    error fix

--- server/src/server.js
+++ server/src/server.js
@@ -33,7 +33,7 @@ app.get('/', (req, res) => {
 if(process.env.NODE_ENV === 'production') {
     app.use(express.static(path.join(__dirname, '../client/dist')));
     app.get('*', (req, res) => {
-        res.sendFile(path.join(__dirname, 'dist', 'index.html'));
+        res.sendFile(path.join(__dirname, '../client/dist/index.html'));
     });
 }
 
