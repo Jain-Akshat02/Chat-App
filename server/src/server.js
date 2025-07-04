fcbae2cdfa585d83a9ced6cc0cd6b1c11ea4a91c
AkshatJain <jainakshat_02@protonmail.com>
2025-07-04 19:01:50 +0530

    ----

--- server/src/server.js
+++ server/src/server.js
@@ -1,49 +1,42 @@
-// server.js
-import express from 'express';
-import dotenv from 'dotenv';
-import cookieParser from 'cookie-parser';
-import cors from 'cors';
-import http from 'http';
+import express from "express";
+import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
+import cors from "cors";
+
 import path from "path";
 
-import { connectDB } from '../config/db.js';
-import authRoutes from '../routes/auth.routes.js';
-import messageRoutes from '../routes/message.routes.js';
+import { connectDB } from "./lib/db.js";
 
-import { initSocket } from '../config/socket.js'; // 🚀 new import
+import authRoutes from "./routes/auth.route.js";
+import messageRoutes from "./routes/message.route.js";
+import { app, server } from "./lib/socket.js";
 
 dotenv.config();
-const app = express();
-const originalUse = app.use.bind(app);
-app.use = (...args) => {
-  if (typeof args[0] === 'string' && args[0].startsWith('http')) {
-    console.error('🚨 Invalid route path passed to app.use():', args[0]);
-  }
-  return originalUse(...args);
-};
-const server = http.createServer(app);
-initSocket(server); // 💥 This replaces io/server logic in socket.js
 
 const PORT = process.env.PORT;
 const __dirname = path.resolve();
 
+app.use(express.json());
 app.use(cookieParser());
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+  })
+);
 
-app.use(cors({
-  origin: "http://localhost:5173",
-  credentials: true
-}));
+app.use("/api/auth", authRoutes);
+app.use("/api/messages", messageRoutes);
 
-app.use('/auth', authRoutes);
-app.use('/message', messageRoutes);
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
 
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '../client/dist')));
-  app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, '../client', 'dist', 'index.html'));
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
   });
 }
 
 server.listen(PORT, () => {
+  console.log("server is running on PORT:" + PORT);
   connectDB();
 });
