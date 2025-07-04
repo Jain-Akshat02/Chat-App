f6d0598eebe292355bfd14f233fa9a47c9be67ba
AkshatJain <jainakshat_02@protonmail.com>
2025-07-04 18:34:14 +0530

    check

--- server/src/server.js
+++ server/src/server.js
@@ -1,6 +1,7 @@
 import dotenv from 'dotenv'
 import cookieParser from 'cookie-parser'
 import cors from 'cors'
+import express from 'express';
 
 import path from "path";
 import { connectDB } from '../config/db.js'
@@ -17,11 +18,6 @@ const __dirname = path.resolve();
 
 app.use(express.json({limit: '50mb'}));
 app.use(cookieParser());
-
-
-
-app.use(express.urlencoded({ limit: '50mb', extended: true })) // Middleware to parse URL-encoded bodies
-console.log("---error1---");
 
 app.use(cors({
     origin: "http://localhost:5173",
