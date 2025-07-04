0226b31fcd1820854bf7af71bd9c3ca1d685cae6
AkshatJain <jainakshat_02@protonmail.com>
2025-07-04 18:01:07 +0530

    tryign to check where s the issue

--- server/src/server.js
+++ server/src/server.js
@@ -10,6 +10,7 @@ import authRoutes from '../routes/auth.routes.js'
 import messageRoutes from '../routes/message.routes.js'
 
 import {app,server,io} from '../config/socket.js'
+import { log } from 'console';
 
 dotenv.config();
 const PORT = process.env.PORT;
@@ -21,15 +22,21 @@ app.use(cookieParser());
 
 
 app.use(express.urlencoded({ limit: '50mb', extended: true })) // Middleware to parse URL-encoded bodies
+console.log("---error1---");
+
 app.use(cors({
     origin: "http://localhost:5173",
     credentials: true, 
 })
-);
-app.use(express.urlencoded({ extended: true }));
 
+);
+console.log("---error2---");
+app.use(express.urlencoded({ extended: true }));
+console.log("---error3---");
 app.use('/auth', authRoutes);
+console.log("---error4---");
 app.use('/message', messageRoutes);
+console.log("---error5---");
 
 if(process.env.NODE_ENV === 'production') {
     app.use(express.static(path.join(__dirname, '../client/dist')));
