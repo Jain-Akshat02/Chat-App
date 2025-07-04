bd7a379c4c7035f12502f81d2ff15423c78f49c8
AkshatJain <jainakshat_02@protonmail.com>
2025-07-04 11:42:02 +0530

    route fix

--- client/src/lib/axios.js
+++ client/src/lib/axios.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 export const axiosInstance = axios.create({
     baseURL: import.meta.env.MODE === 'production'
-        ? "/"
+        ? "https://chat-app-kz3d.onrender.com/"
         : 'http://localhost:5000',
     withCredentials: true,
     headers: {
