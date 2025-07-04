0b3a08bd7ce8c121cf3ee81382b6794d5589661d
AkshatJain <jainakshat_02@protonmail.com>
2025-07-04 15:38:12 +0530

    checking errors for deployment--

--- server/src/server.js
+++ server/src/server.js
@@ -1,21 +1,25 @@
 import express from 'express'
-import authRoutes from '../routes/auth.routes.js'
 import dotenv from 'dotenv'
 import cookieParser from 'cookie-parser'
 import cors from 'cors'
-import { connectDB } from '../config/db.js'
-import messageRoutes from '../routes/message.routes.js'
-dotenv.config();
-import {app,server,io} from '../config/socket.js'
+
 import path from "path";
+import { connectDB } from '../config/db.js'
 
-app.use(cookieParser());
+import authRoutes from '../routes/auth.routes.js'
+import messageRoutes from '../routes/message.routes.js'
 
+import {app,server,io} from '../config/socket.js'
+
+dotenv.config();
 const PORT = process.env.PORT;
-
 const __dirname = path.resolve();
 
-app.use(express.json({limit: '50mb'})) // Middleware to parse JSON bodies
+app.use(express.json({limit: '50mb'}));
+app.use(cookieParser());
+
+
+
 app.use(express.urlencoded({ limit: '50mb', extended: true })) // Middleware to parse URL-encoded bodies
 app.use(cors({
     origin: "http://localhost:5173",
@@ -23,11 +27,10 @@ app.use(cors({
 })
 );
 app.use(express.urlencoded({ extended: true }));
-app.use('/auth', authRoutes)   
+
+app.use('/auth', authRoutes);
 app.use('/message', messageRoutes);
-app.get('/', (req,res)=> {
-    res.send('Welcome to the API')
-});
+
 if(process.env.NODE_ENV === 'production') {
     app.use(express.static(path.join(__dirname, '../client/dist')));
     app.get('*', (req, res) => {
